1ed212528a645d0688aa727c9ed8fe738cc48d23
Kevin Yang <e510021@gmail.com>
2023-07-28 01:03:44 +0800

    Correct Error 75.

--- sorting/75. Sort Colors.js
+++ sorting/75. Sort Colors.js
@@ -2,40 +2,11 @@
 
 // Bubble sort
 var sortColors = function (nums) {
-  if (nums.length <= 1) return nums;
-  const midPoint = Math.floor(nums.length / 2);
-  const leftArr = sortColors(nums.slice(0, midPoint));
-  const rightArr = sortColors(nums.slice(midPoint));
-
-  return merge(leftArr, rightArr);
-
-  function merge(arr1, arr2) {
-    const result = [];
-    let pointer1 = 0;
-    let pointer2 = 0;
-
-    while (pointer1 < arr1.length && pointer2 < arr2.length) {
-      if (arr1[pointer1] < arr2[pointer2]) {
-        result.push(arr1[pointer1]);
-        pointer1++;
-        continue;
-      }
-
-      result.push(arr2[pointer2]);
-      pointer2++;
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length; j++) {
+      if (nums[j] > nums[j + 1])
+        [nums[j + 1], nums[j]] = [nums[j], nums[j + 1]];
     }
-
-    while (pointer1 < arr1.length) {
-      result.push(arr1[pointer1]);
-      pointer1++;
-    }
-
-    while (pointer2 < arr2.length) {
-      result.push(arr2[pointer2]);
-      pointer2++;
-    }
-
-    return result;
   }
 };
 
