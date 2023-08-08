a8f4acc19015e95e0540adef0e4113ce9e3c2992
Kevin Yang <e510021@gmail.com>
2023-08-08 23:56:24 +0800

    Problem 26.

---
+++ 26. Remove Duplicates from Sorted Array.js
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+
+var removeDuplicates = function (nums) {
+  if (nums.length === 0) return 0;
+
+  // the index of element getting manipulated,
+  // start with 1 as the first element will never have to be changed
+  let index = 1;
+
+  // loop through the array to find the intersection of distinct numbers
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] !== nums[i - 1]) {
+      nums[index] = nums[i];
+      index++;
+    }
+  }
+
+  return index;
+};
