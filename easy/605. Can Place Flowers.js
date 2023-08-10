7886efea878e7482eeea6615a795a38319a27ffc
Kevin Yang <e510021@gmail.com>
2023-08-10 23:31:06 +0800

    Problem 605. refactored

--- easy/605. Can Place Flowers.js
+++ easy/605. Can Place Flowers.js
@@ -11,3 +11,21 @@ var canPlaceFlowers = function (flowerbed, n) {
   }
   return n <= 0; // if there are no flowers left to be planted, n will be valid
 };
+
+// refactored solution, not sure if it's better or worse though
+// probably faster? since there are fewer conditions to check for
+var canPlaceFlowers = function (flowerbed, n) {
+  for (let i = 0; i < flowerbed.length; i++) {
+    // if the current position is already planted, skip an iteration
+    if (flowerbed[i] === 1) {
+      i++;
+      continue;
+    }
+    // if the current position is empty and the next is also empty, it's a valid position
+    if (flowerbed[i + 1] !== 1) {
+      i++;
+      n--;
+    }
+  }
+  return n <= 0;
+};
