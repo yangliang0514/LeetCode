18928a41bde871505d6ad26a0195c3b87cbb1191
Kevin Yang <e510021@gmail.com>
2023-08-13 03:02:11 +0800

    Problem 121.

---
+++ easy/121. Best Time to Buy and Sell Stock.js
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+// brute force solution, time: O(n^2), exceeding time limit in leetcode
+var maxProfit = function (prices) {
+  let profit = 0;
+  // try every combination and finds the largest profit
+  for (let i = 0; i < prices.length; i++) {
+    for (let j = i + 1; j < prices.length; j++) {
+      profit = Math.max(profit, prices[j] - prices[i]);
+    }
+  }
+
+  return profit;
+};
+
+// time: O(n), space: O(1)
+var maxProfit = function (prices) {
+  // keep in track of the lowest price and largest profit
+  let minPrice = prices[0];
+  let maxProfit = 0;
+
+  for (let i = 1; i < prices.length; i++) {
+    // update the minimun price
+    if (prices[i] < minPrice) {
+      minPrice = prices[i];
+      continue;
+    }
+    // current price - minimum price is the current largest profit possible
+    // update the maxProfit if the current profit is larget
+    maxProfit = Math.max(profit, prices[i] - minPrice);
+  }
+  return maxProfit;
+};
--- medium/34. Find First and Last Position of Element in Sorted Array.js
+++ medium/34. Find First and Last Position of Element in Sorted Array.js
@@ -11,11 +11,39 @@ var searchRange = function (nums, target) {
       start = i;
       continue;
     }
-
     // finds the first element that's not the target
     if (nums[i] !== target && start !== undefined) return [start, i - 1];
   }
-
   // return -1 if the target is not found
   return [-1, -1];
+};
+
+// double binary search with left and right bias
+var searchRange = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+  let start = -1;
+  let end = -1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] < target) left = mid + 1;
+    if (nums[mid] >= target) right = mid - 1;
+  }
+  if (nums[left] === target) start = left;
+
+  left = 0;
+  right = nums.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] <= target) left = mid + 1;
+    if (nums[mid] > target) right = mid - 1;
+  }
+
+  if (nums[right] === target) end = right;
+
+  return [start, end];
 };
